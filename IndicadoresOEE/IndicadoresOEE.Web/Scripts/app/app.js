013e933b93e58b255ebff893567c0710d79784ff
Cesar Alejandro Arreola Gonzalez <Caarreola@corpcab.com.mx>
2018-11-26 10:50:35 -0600

    V3

---
+++ IndicadoresOEE/IndicadoresOEE.Web/Scripts/app/app.js
@@ -0,0 +1,64 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('indicadoresoeeapp', ['ngMaterial', 'ngMessages', 'ngSanitize', 'angularMoment'])
+
+        .config(['$mdIconProvider', '$mdDateLocaleProvider', '$mdThemingProvider', '$routeProvider', '$urlRouterProvider', '$stateProvider',
+            function ($mdIconProvider, $mdDateLocaleProvider, $mdThemingProvider, $routeProvider, $urlRouterProvider, $stateProvider) {
+                $mdIconProvider.icon('md-close', '../img/icons/ic_close_24px.svg', 24);
+
+                // Example of a Spanish localization.
+                $mdDateLocaleProvider.months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+                $mdDateLocaleProvider.shortMonths = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+                    'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+                $mdDateLocaleProvider.days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
+                $mdDateLocaleProvider.shortDays = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'];
+                // Can change week display to start on Monday.
+                $mdDateLocaleProvider.firstDayOfWeek = 1;
+
+                $mdDateLocaleProvider.weekNumberFormatter = function (weekNumber) {
+                    return 'Semana ' + weekNumber;
+                };
+                $mdDateLocaleProvider.msgCalendar = 'Calendario';
+                $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
+
+                $mdDateLocaleProvider.formatDate = function (date) {
+                    if (date !== undefined && date !== null && date !== '') {
+                        var day = date.getDate();
+                        var monthIndex = date.getMonth();
+                        var year = date.getFullYear();
+
+                        return day + '/' + (monthIndex + 1) + '/' + year;
+                    }
+
+                    return '';
+                };
+                
+                //$urlRouterProvider.otherwise("inicio");
+
+                //$stateProvider
+                //    .state('inicio', {
+                //        url: "/",
+                //        templateUrl: "/Inicio/Inicio"
+                //    })
+                //    .state('crear-indicador', {
+                //        url: "/indicador/captura-individual",
+                //        templateUrl: "/Indicadores/Captura",
+                //        controller: "CapturaIndicadoresController"
+                //    });
+
+                ////$routeProvider
+                ////    .when('/', {
+                ////        templateUrl: '/Inicio/Inicio',
+                ////        controller: ''
+                ////    })
+                ////    .when('/indicadores/captura-individual', {
+                ////        templateUrl: '/Indicadores/Captura',
+                ////        controller: 'CapturaIndicadoresController'
+                ////    })
+                ////    .otherwise({
+                ////        redirectTo: '/'
+                ////    });
+            }]);
+})();
---
+++ IndicadoresOEE/IndicadoresOEE.Web/Scripts/app/controllers/CapturaIndicadoresController.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('indicadoresoeeapp')
+        .controller('CapturaIndicadoresController', CapturaIndicadoresController);
+    
+    CapturaIndicadoresController.$inject = ['$scope', '$timeout', '$location', '$anchorScroll', '$log', '$http', '$window', '$compile', '$mdToast', '$mdDialog'];
+
+    function CapturaIndicadoresController($scope, $timeout, $location, $anchorScroll, $log, $http, $window, $compile, $mdToast, $mdDialog)
+    {
+        $scope.DatosGenerales = { IndiceCentro: '' };
+
+        $scope.ListaCentros = [
+            { Nombre: 'PI01', Indice: 1 },
+            { Nombre: 'PI02', Indice: 2 },
+            { Nombre: 'PI03', Indice: 3 },
+            { Nombre: 'PI04', Indice: 4 },
+            { Nombre: 'PI05', Indice: 5 },
+            { Nombre: 'PI06', Indice: 6 }
+        ];
+
+        $scope.ListaDepartamentos = [
+            { Nombre: 'PI01', Indice: 1, IndiceCentro: 1 },
+            { Nombre: 'PI02', Indice: 2, IndiceCentro: 1},
+            { Nombre: 'PI03', Indice: 3, IndiceCentro: 1},
+            { Nombre: 'PI04', Indice: 4, IndiceCentro: 1},
+            { Nombre: 'PI05', Indice: 5, IndiceCentro: 1},
+            { Nombre: 'PI06', Indice: 6, IndiceCentro: 1}
+        ];
+
+        $scope.ListaDepartamentos = [
+            { Nombre: 'PI01', Indice: 1, IndiceCentro: 1 },
+            { Nombre: 'PI02', Indice: 2, IndiceCentro: 1},
+            { Nombre: 'PI03', Indice: 3, IndiceCentro: 1},
+            { Nombre: 'PI04', Indice: 4, IndiceCentro: 1},
+            { Nombre: 'PI05', Indice: 5, IndiceCentro: 1},
+            { Nombre: 'PI06', Indice: 6, IndiceCentro: 1}
+        ];
+
+        $scope.ResetearTerminoBusqueda = function () {
+            $scope.TerminoBusqueda = '';
+        };
+    }
+}) ();
