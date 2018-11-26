f0e7cbe70f4b4a1d293536ad002def2572102fb5
Cesar Alejandro Arreola Gonzalez <Caarreola@corpcab.com.mx>
2018-11-26 17:07:44 -0600

    5pm

--- IndicadoresOEE/IndicadoresOEE.Web/Scripts/app/controllers/CapturaIndicadorController.js
+++ IndicadoresOEE/IndicadoresOEE.Web/Scripts/app/controllers/CapturaIndicadorController.js
@@ -9,7 +9,18 @@
 
     function CapturaIndicadorController($scope, $timeout, $location, $anchorScroll, $log, $http, $window, $compile, $mdToast, $mdDialog)
     {
-        $scope.DatosGenerales = { IndiceCentro: null, IndiceDepartamento: null, IndiceLinea: null, IndiceProceso: null, IndiceVelocidad: null };
+        $scope.DatosGenerales = {
+            IndiceCentro: null,
+            IndiceDepartamento: null,
+            IndiceLinea: null,
+            IndiceProceso: null,
+            IndiceVelocidad: null,
+            Orden: null,
+            Lote: null,
+            Material: null,
+            Descripcion: null,
+            Velocidad: null
+        };
         
         $scope.ResetearTerminoBusqueda = function () {
             $scope.TerminoBusqueda = '';
@@ -25,18 +36,23 @@
                         var Mensaje = response.data.Mensaje;
                         $log.info('Se produjo el siguiente error en el método ObtenerCentros = ' + Mensaje);
                     }
-                    else
+                    else {
                         $scope.ListaCentros = response.data.ListaCentros;
+                        $scope.DatosGenerales.IndiceCentros = $scope.ListaCentros.length === 1 ? $scope.ListaCentros[0].Indice : null;
+
+                        if ($scope.DatosGenerales.IndiceCentros !== null)
+                            $scope.ObtenerDepartamentos();
+                    }
                 },
                 function (response) {
-                    $log.info('Hubo un error: ' + response);
+                    $log.info('Hubo un error: Estatus = ' + response.status + ', Error = ' + response.data);
                 })
                 .catch(function (response) {
                     $log.info('Excepcion: ', response);
                     throw e;
                 })
                 .finally(function () {
-                    $log.info('Finally');
+                    $log.info('Método ObtenerCentros finalizado');
                 });
         };
 
@@ -53,24 +69,27 @@
                     else {
                         $scope.ListaDepartamentos = response.data.ListaDepartamentos;
                         $scope.DatosGenerales.IndiceDepartamento = $scope.ListaDepartamentos.length === 1 ? $scope.ListaDepartamentos[0].Indice : null;
+
+                        if ($scope.DatosGenerales.IndiceCentros !== null)
+                            $scope.ObtenerLineas();
                     }
                 },
                 function (response) {
-                    $log.info('Hubo un error: ' + response);
+                    $log.info('Hubo un error: Estatus = ' + response.status + ', Error = ' + response.data);
                 })
                 .catch(function (response) {
                     $log.info('Excepcion: ', response);
                     throw e;
                 })
                 .finally(function () {
-                    $log.info('Finally');
+                    $log.info('Método ObtenerDepartamentos finalizado');
                 });
         };
 
         $scope.ObtenerLineas = function ()
         {
             $http
-                .post('/Linea/ObtenerLineas', JSON.stringify({ IndiceLinea: $scope.DatosGenerales.IndiceLinea }))
+                .post('/Linea/ObtenerLineas', JSON.stringify({ IndiceDepartamento: $scope.DatosGenerales.IndiceDepartamento }))
                 .then(function (response) {
                     var Estado = response.data.Estado;
                     if (!Estado) {
@@ -80,24 +99,27 @@
                     else {
                         $scope.ListaLineas = response.data.ListaLineas;
                         $scope.DatosGenerales.IndiceLinea = $scope.ListaLineas.length === 1 ? $scope.ListaLineas[0].Indice : null;
+
+                        if ($scope.DatosGenerales.IndiceCentros !== null)
+                            $scope.ObtenerProcesos();
                     }
                 },
                 function (response) {
-                    $log.info('Hubo un error: ' + response);
+                    $log.info('Hubo un error: Estatus = ' + response.status + ', Error = ' + response.data);
                 })
                 .catch(function (response) {
                     $log.info('Excepcion: ', response);
                     throw e;
                 })
                 .finally(function () {
-                    $log.info('Finally');
+                    $log.info('Método ObtenerLineas finalizado');
                 });
         };
 
         $scope.ObtenerProcesos = function ()
         {
             $http
-                .post('/Procesos/ObtenerProcesos', JSON.stringify({ IndiceProceso: $scope.DatosGenerales.IndiceProceso }))
+                .post('/Proceso/ObtenerProcesos', JSON.stringify({ IndiceLinea: $scope.DatosGenerales.IndiceLinea }))
                 .then(function (response) {
                     var Estado = response.data.Estado;
                     if (!Estado) {
@@ -107,18 +129,43 @@
                     else {
                         $scope.ListaProcesos = response.data.ListaProcesos;
                         $scope.DatosGenerales.IndiceProceso = $scope.ListaProcesos.length === 1 ? $scope.ListaProcesos[0].Indice : null;
+                        
                     }
                 },
                 function (response) {
-                    $log.info('Hubo un error: ' + response);
+                    $log.info('Hubo un error: Estatus = ' + response.status + ', Error = ' + response.data);
                 })
                 .catch(function (response) {
                     $log.info('Excepcion: ', response);
                     throw e;
                 })
                 .finally(function () {
-                    $log.info('Finally');
+                    $log.info('Método ObtenerProcesos finalizado');
                 });
         };
+
+
+        $scope.$watch('DatosGenerales.IndiceCentro', function (newValue, oldValue) {
+            if (newValue !== undefined && newValue !== null && newValue !== '') {
+                $scope.ObtenerDepartamentos();
+            }
+        });
+
+        $scope.$watch('DatosGenerales.IndiceDepartamento', function (newValue, oldValue) {
+            if (newValue !== undefined && newValue !== null && newValue !== '') {
+                $scope.ObtenerLineas();
+            }
+        });
+
+        $scope.$watch('DatosGenerales.IndiceLinea', function (newValue, oldValue) {
+            if (newValue !== undefined && newValue !== null && newValue !== '') {
+                $scope.ObtenerProcesos();
+            }
+        });
+
+        $scope.$watch('DatosGenerales.IndiceProceso', function (newValue, oldValue) {
+            if (newValue !== undefined && newValue !== null && newValue !== '') {
+            }
+        });
     }
 }) ();
